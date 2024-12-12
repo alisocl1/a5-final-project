70678fc4e92d3a79680eeb64e21cae1c281b0194
Jacqueline Nguyen <jacqun11@uci.edu>
2024-12-11 22:55:06 -0800

    final + comment out reset button

--- src/assets/user-name/user-name.jsx
+++ src/assets/user-name/user-name.jsx
@@ -4,14 +4,14 @@ import './user-name.css'
 const UserNameInput = ({ userName, setUserName, onNext }) => {
     return (
         <div className="name-input-container">
-            <h1>welcome, what's your name?</h1>
+            <h1>welcome to spacedump!</h1>
             <div className="name-input-row">
                 <label htmlFor="user-name-input" className="sr-only">enter your first name</label>
                 <input
                     id="user-name-input"
                     type="text"
                     className = "line-input"
-                    placeholder="enter your first name"
+                    placeholder="what's your name?"
                     value={userName}
                     onChange={(e) => setUserName(e.target.value)}
                 />
